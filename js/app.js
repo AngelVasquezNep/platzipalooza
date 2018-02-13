ad6ff47c87f24bee527ce562e8e458c44ba9f348
Ángel Vásquez <angel.vasquez.unam@gmail.com>
2018-02-13 01:44:42 -0600

    Versión estable

--- js/app.js
+++ js/app.js
@@ -1,1 +1,17 @@
 $(document).foundation()
+
+$('[data-toggle-dia]').click(function (ev){
+  const panel = $(this).data('toggle-dia')
+  $('#lineup-tabs').foundation('selectTab', panel)
+})
+
+
+const $offCanvas = $('#offCanvas')
+const $sticky = $('#sticky')
+
+$offCanvas.find('li').click(function (ev){
+  $offCanvas.foundation('close')
+  setTimeout(() => {
+    $sticky.css('left', '0px')
+  }, 0);
+})
